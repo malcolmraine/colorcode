e529107ba4072c875cc79c73b6a9602941e90871
Malcolm Hall <malcolmrainehall@gmail.com>
2026-03-02 13:52:58 -0500

    remove test.py file

--- test.py
+++
@@ -1,8 +0,0 @@
-import colorcode
-
-start = colorcode.Color(255, 255, 255)
-end = colorcode.Color(0, 255, 255)
-g = colorcode.gradient.Gradient(start, end, curve=colorcode.gradient.gr)
-
-for i in range(0, 100):
-    print(g.get_color(i / 100))
