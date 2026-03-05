cf5f2083d7ccc8a19c84f532259ce6d5af186eea
Malcolm Hall <malcolmrainehall@gmail.com>
2026-03-04 20:38:28 -0500

    Add svg exporter class

---
+++ colorcode/util/svg_exporter.py
@@ -0,0 +1,106 @@
+"""
+File: colorcode/util/svg_exporter.py
+Description: Exports colors to an SVG color strip.
+Author: Malcolm Hall
+License: MIT
+"""
+
+from __future__ import annotations
+
+from string import Template
+import typing
+import pathlib
+
+if typing.TYPE_CHECKING:
+    from colorcode.color import Color
+
+
+svg_template = Template("""<?xml version="1.0" encoding="iso-8859-1"?>
+
+<svg version="1.1" id="Layer_1" 
+     xmlns="http://www.w3.org/2000/svg" 
+     x="0px" y="0px"
+	 viewBox="0 0 $WIDTH $HEIGHT" 
+     style="enable-background:new 0 0 $WIDTH $HEIGHT;"
+     xml:space="preserve"
+     $CRISP_SHAPE_RENDERING
+     >
+$SLICE_FILL
+</svg>
+""")
+
+slice_template = Template(
+    '<rect x="$X_POS" y="0" style="fill:rgba($RED, $GREEN, $BLUE, $ALPHA);" width="$SLICE_WIDTH" height="$HEIGHT"/>'
+)
+
+
+class SvgExporter(object):
+    def __init__(
+        self, color_list: list[Color], image_width: int = 512, image_height: int = 128
+    ) -> None:
+        self.color_list = color_list
+        self.image_width = image_width
+        self.image_height = image_height
+        self.crisp_shape_rendering = True
+        self._slice_width = int(self.image_width / len(self.color_list))
+
+    def is_valid(self) -> bool:
+        if (
+            len(self.color_list) == 0
+            or self._slice_width <= 0
+            or self.image_height <= 0
+            or self.image_width <= 0
+        ):
+            return False
+        return True
+
+    def save(self, filename: pathlib.Path | str) -> None:
+        slice_xml = ""
+        x_pos = 0
+
+        for color in self.color_list:
+            r, g, b, a = color.rgba
+            slice_xml += (
+                "    "
+                + slice_template.substitute(
+                    {
+                        "SLICE_WIDTH": str(self._slice_width),
+                        "HEIGHT": str(self.image_height),
+                        "RED": str(round(r)),
+                        "GREEN": str(round(g)),
+                        "BLUE": str((round(b))),
+                        "ALPHA": str(round(a / color.base)),
+                        "X_POS": str(x_pos),
+                    }
+                )
+                + "\n"
+            )
+            x_pos += self._slice_width
+
+        svg_dict = {
+            "WIDTH": str(self.image_width),
+            "HEIGHT": str(self.image_height),
+            "SLICE_FILL": slice_xml,
+        }
+
+        if self.crisp_shape_rendering:
+            svg_dict["CRISP_SHAPE_RENDERING"] = 'shape-rendering="crispEdges"'
+        else:
+            svg_dict["CRISP_SHAPE_RENDERING"] = ""
+        svg_xml = svg_template.substitute(svg_dict)
+
+        with open(str(filename), "w") as f:
+            f.write(svg_xml)
+
+
+
+
+from colorcode.color import Color
+from colorcode import Gradient
+from colorcode import gradient_curve
+from colorcode import color_model
+
+gradient = Gradient(Color(0, 0, 0), Color(128, 50, 255), steps=100, model=color_model.HSV_Model(),curve=gradient_curve.AgnesiWitchCurve())
+colorList = list(gradient)
+exporter = SvgExporter([Color(128, 120, 0)], image_width=512)
+exporter.save(pathlib.Path(__file__).parent / "color.svg")
